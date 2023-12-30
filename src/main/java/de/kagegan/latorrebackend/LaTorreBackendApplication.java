5e6123bb8d562962886b5bf9b96e3dc4237ec595
unknown <kagegan1337@gmail.com>
2023-12-30 20:31:36 +0100

    initial

---
+++ src/main/java/de/kagegan/latorrebackend/LaTorreBackendApplication.java
@@ -0,0 +1,13 @@
+package de.kagegan.latorrebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaTorreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaTorreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kagegan/latorrebackend/LaTorreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kagegan.latorrebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaTorreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
